35166643bdd61e79782815f7cac2c867fab4ce44
halil <156532556+halillsahin@users.noreply.github.com>
2024-02-02 18:03:10 +0300

    Add files via upload

---
+++ projecteuler15.py
@@ -0,0 +1,4 @@
+import math
+a=math.factorial(40)
+b=math.factorial(20)
+print(a/(b*b))             #20 adım sola 20 adım aşağı atıldığı için toplamda 40 adım atılır ve c(40,20) kombinasyonu hesaplanır.
